da8a16ffc0cdbe851d40b83180b16a91faab55bf
renan22rsw <renan22rsw@gmail.com>
2026-05-12 03:50:09 -0300

    delete: app controller/service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { DatabaseService } from './database/database.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -9,7 +8,6 @@ import { StockMovementsModule } from './stock-movements/stock-movements.module';
 
 @Module({
   imports: [AuthModule, UsersModule, ProductsModule, StockMovementsModule],
-  controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [DatabaseService],
 })
 export class AppModule {}
