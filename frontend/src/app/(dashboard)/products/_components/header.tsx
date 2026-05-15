06f63efea934dd8ab17ef62cc6f79588ba33b552
renan22rsw <renan22rsw@gmail.com>
2026-05-15 15:06:39 -0300

    feat:products header (dialog needs to be implemented)

---
+++ frontend/src/app/(dashboard)/products/_components/header.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search, Plus } from "lucide-react";
+
+export const ProductsHeader = () => {
+  return (
+    <header className="px-6 py-8">
+      <div className="items-center justify-between xl:flex">
+        <div className="py-4">
+          <h1 className="text-xl font-bold xl:text-2xl">Produtos</h1>
+          <p className="text-muted-foreground">
+            Gerencie os produtos em seu estoque
+          </p>
+        </div>
+
+        <div className="relative flex items-center gap-4">
+          <Search className="text-muted-foreground absolute top-2.5 left-3 size-4" />
+
+          <Input
+            placeholder="Buscar produto ou SKU..."
+            className="max-w-80 rounded-lg py-4.5 pl-10 placeholder:text-xs"
+          />
+
+          <Button className="px-4 py-4.5">
+            {" "}
+            {/* Dialog will be implement soon */}
+            <Plus />
+            Novo Produto
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ frontend/src/app/(dashboard)/products/page.tsx
@@ -0,0 +1,11 @@
+import { ProductsHeader } from "./_components/header";
+
+const Products = () => {
+  return (
+    <>
+      <ProductsHeader />
+    </>
+  );
+};
+
+export default Products;
