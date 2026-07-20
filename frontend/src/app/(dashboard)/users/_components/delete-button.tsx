fd2266ae35dc96c9a8714fb291ecd9776fbc9355
renan22rsw <renan22rsw@gmail.com>
2026-07-20 20:15:44 -0300

    feat: enhance delete user functionality with loading state and permission checks

--- frontend/src/app/(dashboard)/users/_components/delete-button.tsx
+++ frontend/src/app/(dashboard)/users/_components/delete-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   AlertDialog,
   AlertDialogAction,
@@ -9,9 +11,56 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Spinner } from "@/components/ui/spinner";
+import { User } from "@/types/user";
 import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 
-export const DeleteUserButton = () => {
+interface DeleteUsersProps {
+  id: string;
+  currentUser: User;
+}
+
+export const DeleteUserButton = ({ id, currentUser }: DeleteUsersProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const hasPermission = currentUser.role !== "ADMIN";
+
+  const handleDelete = async (id: string) => {
+    try {
+      setIsLoading(true);
+
+      if (currentUser.id === id) {
+        toast.error("Você não pode se auto deletar");
+        return;
+      }
+
+      const response = await fetch(`/api/admin/users/${id}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Algo deu errado o usuário");
+      }
+
+      toast.success("Usuário deletado com sucesso", {
+        description: "O usuário foi deletado com sucesso",
+      });
+      router.refresh();
+    } catch (err) {
+      toast.error("Error ao deletar o usuário", {
+        description:
+          err instanceof Error ? err.message : "Tente novamente mais tarde",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -29,7 +78,20 @@ export const DeleteUserButton = () => {
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancelar</AlertDialogCancel>
-          <AlertDialogAction>Deletar</AlertDialogAction>
+
+          <AlertDialogAction
+            onClick={() => handleDelete(id)}
+            disabled={isLoading || hasPermission}
+          >
+            {isLoading ? (
+              <>
+                <Spinner />
+                Deletando...
+              </>
+            ) : (
+              "Deletar"
+            )}
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
